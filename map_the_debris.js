9ccc8f1117b5f7b2d72bf41ccaf976614e3d6df2
Frank <fgrullon@users.noreply.github.com>
2017-05-10 16:07:38 -0400

    comments and variables
    create variables to handle some numbers to make the code more readable, and add some comments to explain the process follow

--- map_the_debris.js
+++ map_the_debris.js
@@ -3,10 +3,22 @@ function orbitalPeriod(arr) {
   var earthRadius = 6367.4447;
 
   for (var i = 0; i < arr.length; i++) {
-
-  	var period = (Math.PI * 2) * Math.sqrt((Math.pow((earthRadius + arr[i].avgAlt), 3) / GM));
-
+    var avgAlt = arr[i].avgAlt;
+    
+    /*We used the formula 
+    T = 2π √a^3 / gm
+    
+    -- Simbols --
+    T = Orbital Period
+    a = the sum of the Earth radius plus average altitude
+    gm = gravitational parameter
+    
+    */
+  	var period = (Math.PI * 2) * Math.sqrt((Math.pow((earthRadius + avgAlt), 3) / GM));
+    
+    //We delete de avgAlt property inside the object because we don't need it in the future
   	delete arr[i].avgAlt;
+    // Now we add the orbitalPeriod to the object and go to check if is another element in the object to calculate
   	arr[i]['orbitalPeriod'] = Math.round(period);
   };
 
